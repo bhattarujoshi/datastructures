4c16c2a8f7badfaaa352694280c889c1a84ff600
bhattarujoshi <117716007+bhattarujoshi@users.noreply.github.com>
2023-05-18 12:12:06 +0530

    NNUMINRE.C

---
+++ NNUMINRE.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+{
+    int i;
+    printf("The first 10 natural numbers are:\n");
+    for (i = 10; i >= 1; i--) 
+	{
+        printf("%d ", i);
+    }
+    printf("\n");
+	return 0;
+}
